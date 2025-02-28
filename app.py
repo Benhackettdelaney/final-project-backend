2e1fbbfbf3b204c63ecade8fa37fdd4d3ee5dd4d
Ben Hackett Delaney <96736561+Benhackettdelaney@users.noreply.github.com>
2025-02-28 09:05:38 +0000

    Add files via upload
    added the SQLite database and added the columns

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from routes.movie_routes import movie_bp
+from extensions import db, migrate
+from config.config import Config
+from models.movie import Movie 
+from models.user import User 
 
 app = Flask(__name__)
+
+
+app.config.from_object(Config)
+
+
+db.init_app(app)
+migrate.init_app(app, db)
+
+
+app.register_blueprint(movie_bp, url_prefix='/api')
 
 @app.route('/', methods=['GET'])
 def hello_world():
     return "Hello World"
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
